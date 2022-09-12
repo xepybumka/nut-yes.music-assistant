0cadee066d282191a924e17d50a1d2e50a6af97a
xepybumka <xepybumka@yandex.ru>
2022-09-12 19:28:46 +0400

    added Author base layout

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function all()
+    {
+        $authorModel = new Author();
+        $autors = $authorModel->all();
+        var_dump($autors);
+        $title = 'Авторы';
+        return view(
+            'author.all',
+            [
+                'title' => $title,
+                'authors' => $autors
+            ]
+        );
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,10 @@ Route::get('/album/edit', [AlbumController::class, 'edit'])->name('album-edit');
 Route::get('/album/create/{id}', [AlbumController::class, 'create'])->name('album-create');
 Route::get('/album/delete/{id}', [AlbumController::class, 'delete'])->name('album-delete');
 
+//Authors
+Route::get('/authors', [AuthorController::class, 'all'])->name('authors-all');
+Route::get('/author/{id}', [AuthorController::class, 'get'])->name('author-get');
+Route::get('/author/edit/{id}', [AuthorController::class, 'edit'])->name('author-edit');
+Route::get('/author/create', [AuthorController::class, 'create'])->name('author-create');
+Route::get('/author/delete/{id}', [AuthorController::class, 'delete'])->name('author-delete');
 require __DIR__.'/auth.php';
